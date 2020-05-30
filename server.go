6c21e862f53cd7ae7b3715112b035030d605e23e
M. Hyvarinen <54714032+marhyvar@users.noreply.github.com>
2020-05-30 22:05:01 +0300

    Refactor names

--- server.go
+++ server.go
@@ -12,53 +12,48 @@ import (
 )
 
 type Medicine struct {
-	Id string `json:"Id"`
-	Name string `json:"Name"`
+	Vnr string `json:"Vnr"`
+	ActiveIngredient string `json:"ActiveIngredient"`
 	CommercialName string `json:"CommercialName"`
 	Concentration float64 `json:"Concentration"`
 	Volume int `json:"Volume"`
-	Dosage float64 `json:"Dosage"`
-	GroupId string `json:"GroupId"`
+	Atc string `json:"Atc"`
 }
 
 type GroupMedicines []Medicine
 
 var medGroup = GroupMedicines{
 	{
-		Id: "1",
-		Name: "kefaleksiini",
+		Vnr: "398362",
+		ActiveIngredient: "kefaleksiini",
 		CommercialName: "Kefexin",
 		Concentration: 100.0,
 		Volume: 50,
-		Dosage: 50.0,
-		GroupId: "1",
+		Atc: "J01DB01",
 	},
 	{
-		Id: "2",
-		Name: "kefaleksiini",
+		Vnr: "581900",
+		ActiveIngredient: "kefaleksiini",
 		CommercialName: "Kefexin",
 		Concentration: 50.0,
 		Volume: 100,
-		Dosage: 50.0,
-		GroupId: "1",
+		Atc: "J01DB01",
 	},
 	{
-		Id: "3",
-		Name: "amoksisilliini",
+		Vnr: "006350",
+		ActiveIngredient: "amoksisilliini",
 		CommercialName: "Amorion",
-		Concentration: 100.0,
-		Volume: 30,
-		Dosage: 40.0,
-		GroupId: "2",
+		Concentration: 50.0,
+		Volume: 100,
+		Atc: "J01CA04",
 	},
 	{
-		Id: "4",
-		Name: "amoksisilliini",
+		Vnr: "006299",
+		ActiveIngredient: "amoksisilliini",
 		CommercialName: "Amorion",
 		Concentration: 100.0,
 		Volume: 50,
-		Dosage: 40.0,
-		GroupId: "2",
+		Atc: "J01CA04",
 	},
 }
 
@@ -67,10 +62,10 @@ func home(w http.ResponseWriter, r *http.Request) {
 }
 
 func getMedicine(w http.ResponseWriter, r *http.Request) {
-	medId := mux.Vars(r)["id"]
+	medVnr := mux.Vars(r)["vnr"]
 
 	for _, med := range medGroup {
-		if med.Id == medId {
+		if med.Vnr == medVnr {
 			json.NewEncoder(w).Encode(med)
 		}
 	}
@@ -92,22 +87,22 @@ func createMedicine(w http.ResponseWriter, r *http.Request) {
 
 func deleteMedicine(w http.ResponseWriter, r *http.Request) {
     vars := mux.Vars(r)
-    id := vars["id"]
+    Vnr := vars["vnr"]
 
     for index, medicine := range medGroup {
-        if medicine.Id == id {
-			//if id matches, removes the medicine from medGroup
+        if medicine.Vnr == Vnr {
+			//if Vnr matches, removes the medicine from medGroup
             medGroup = append(medGroup[:index], medGroup[index+1:]...)
         }
     }
 
 }
 
-func getByGroupId(w http.ResponseWriter, r *http.Request) {
-	groupId := mux.Vars(r)["id"]
+func getByAtc(w http.ResponseWriter, r *http.Request) {
+	Atc := mux.Vars(r)["atc"]
 	var group []Medicine
 	for _, med := range medGroup {
-		if med.GroupId == groupId {
+		if med.Atc == Atc {
 			group = append(group, med)
 		}
 	}
@@ -115,7 +110,7 @@ func getByGroupId(w http.ResponseWriter, r *http.Request) {
 }
 
 func updateMedicine(w http.ResponseWriter, r *http.Request) {
-	id := mux.Vars(r)["id"]
+	Vnr := mux.Vars(r)["vnr"]
 	var changedMedicine Medicine
 
 	reqBody, err := ioutil.ReadAll(r.Body)
@@ -125,14 +120,13 @@ func updateMedicine(w http.ResponseWriter, r *http.Request) {
 	json.Unmarshal(reqBody, &changedMedicine)
 
 	for index, medicine := range medGroup {
-		if medicine.Id == id {
-			medicine.Name = changedMedicine.Name
+		if medicine.Vnr == Vnr {
+			medicine.ActiveIngredient = changedMedicine.ActiveIngredient
 			medicine.CommercialName = changedMedicine.CommercialName
 			medicine.Concentration = changedMedicine.Concentration
 			medicine.Volume = changedMedicine.Volume
-			medicine.Dosage = changedMedicine.Dosage
-			medicine.GroupId = changedMedicine.GroupId
-			// if id matches, updates medGroup[index] with new value
+			medicine.Atc = changedMedicine.Atc
+			// if Vnr matches, updates medGroup[index] with new value
 			medGroup = append(medGroup[:index], medicine)
 			json.NewEncoder(w).Encode(medicine)
 		}
@@ -143,11 +137,11 @@ func requestHandler() {
 	router := mux.NewRouter().StrictSlash(true)
 	router.HandleFunc("/", home)
 	router.HandleFunc("/medicines", getAllMedicines).Methods("GET")
-	router.HandleFunc("/medicines/{id}", getMedicine).Methods("GET")
+	router.HandleFunc("/medicines/{vnr}", getMedicine).Methods("GET")
 	router.HandleFunc("/medicines", createMedicine).Methods("POST")
-	router.HandleFunc("/medicines/{id}", deleteMedicine).Methods("DELETE")
-	router.HandleFunc("/medicines/{id}", updateMedicine).Methods("PUT")
-	router.HandleFunc("/groups/{id}/medicines", getByGroupId).Methods("GET")
+	router.HandleFunc("/medicines/{vnr}", deleteMedicine).Methods("DELETE")
+	router.HandleFunc("/medicines/{vnr}", updateMedicine).Methods("PUT")
+	router.HandleFunc("/groups/{atc}/medicines", getByAtc).Methods("GET")
 	log.Fatal(http.ListenAndServe(":8080", router))
 }
 
